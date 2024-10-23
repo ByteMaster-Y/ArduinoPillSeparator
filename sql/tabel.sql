411e8330649d4e9cfce85666b4bd05310cf1c33e
임예은 <122524095+latteroong@users.noreply.github.com>
2024-10-23 12:32:49 +0900

    db sql문

---
+++ sql/tabel.sql
@@ -0,0 +1,26 @@
+drop table if exists member;
+create table member (
+	pkid int primary key auto_increment,
+    user_id varchar(20) not null,
+    user_pw varchar(20) not null,
+    name varchar(50) not null,
+    regdate timestamp default current_timestamp
+);
+
+drop table if exists alarm;
+create table alarm (
+	pkid int primary key auto_increment,
+    fg_user int,
+    foreign key (fg_user) references member(pkid),
+    pillA int not null,
+    pillB int not null,
+    pillC int not null,
+    pillD int not null,
+    time TIME not null default "00:00:00",
+    day varchar(20)
+);
+
+insert into member(user_id, user_pw, name) values ('iop0512', '1234', '임예은');
+insert into alarm(fg_user, pillA, pillB, pillC, pillD, time, day) values(1, 0, 0, 0, 1, "12:00:00", "월요일");
+select user_id, user_pw, name from member where user_id = 'iop0512' and user_pw = '1234';
+select * from alarm;
