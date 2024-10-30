18386d2d344c5e758c211b18e4e1ccf9c64959b7
임예은 <122524095+latteroong@users.noreply.github.com>
2024-10-30 10:43:57 +0900

    가입기능구현(멤버테이블에 추가)

--- router/member.js
+++ router/member.js
@@ -6,5 +6,6 @@ router.get('/login', controller.login); // member/login.html
 router.post('/login', controller.loginProc); // member.login.html
 router.get('/signup', controller.signup); // member/login.html
 router.post("/cheackUserId", controller.cheackUserId);
+router.post("/register", controller.postRegister);
 
 module.exports = router;
